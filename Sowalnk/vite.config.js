95aec90bea4a5a8d69c96490a3e96fd21fd60c12
EC2 Default User <ec2-user@ip-10-0-22-97.us-east-2.compute.internal>
2025-03-11 06:45:50 +0000

    Resolved merge conflicts

--- Sowalnk/vite.config.js
+++ Sowalnk/vite.config.js
@@ -1,17 +1,24 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
-// https://vite.dev/config/
+// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
   server: {
     host: "0.0.0.0",
+    port: 5173,
+    strictPort: true,
+    allowedHosts: [
+      "sakthidev.site",
+      "3-tier-540623662.us-east-2.elb.amazonaws.com"
+    ],
     proxy: {
       "/api": {
-        target: "http://app-ALB-990835184.us-east-2.elb.amazonaws.com",
+        target: "https://3-tier-540623662.us-east-2.elb.amazonaws.com", // Added 'http://' 
         changeOrigin: true,
-        secure: false, // Only needed if using HTTPS and self-signed certificates
+        secure: false, // Keep false unless using valid HTTPS certificates
       },
     },
   },
 });
+
